e7fdadac28afd61a1e074f786925c9e8802a2b26
Sean Brown <seanbrown82@hotmail.com>
2023-03-19 22:28:04 +0000

    Initialise firebase and folder structure

---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxTouE7uNd1wYR3MkAtLIWTaKkAJNI5bw",
+  authDomain: "vite-todo-b7254.firebaseapp.com",
+  projectId: "vite-todo-b7254",
+  storageBucket: "vite-todo-b7254.appspot.com",
+  messagingSenderId: "239686878920",
+  appId: "1:239686878920:web:f4891138e8dffd4c9f07fd"
+};
+
+const app = initializeApp(firebaseConfig);
